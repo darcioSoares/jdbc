0120caa354443c5344af3fe3ca124be9747422fb
darciosoares <darciosoares28@gmail.com>
2023-06-22 11:25:40 -0300

    Criando banco com jdbc

--- src/jdbc/CriarBanco.java
+++ src/jdbc/CriarBanco.java
@@ -3,19 +3,22 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class TesteConexao {
-	public static void main(String[] args) throws SQLException {
-		//usar sempre os import de java.sql 
-		
-		//verifyServerCertificate=false&useSSL=true -> estou dizendo para fazer um conexao segura ssl mas não checar o cert
+public class CriarBanco {
+	
+	public static void main(String[] args) throws SQLException {		
 		String stringConexao = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
 		String user = "root";
 		String senha = "sousa";
 		
 		Connection conexao = DriverManager.getConnection(stringConexao, user, senha);
 		
-		System.out.println("Conexao efetuada com sucesso");
+		Statement stmt = conexao.createStatement();
+		stmt.execute("CREATE DATABASE IF NOT EXISTS jdbc_db");
+		System.out.println("Banco criado com sucesso");
+		
+		
 		conexao.close();
 		
 	}
