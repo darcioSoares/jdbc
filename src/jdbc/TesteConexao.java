7d5ee384112d73854bda754912d063b69155895b
darciosoares <darciosoares28@gmail.com>
2023-06-22 11:16:17 -0300

    Removendo a pasta bin do git e finalizando conexao com db mysql

--- src/jdbc/TesteConexao.java
+++ src/jdbc/TesteConexao.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class TesteConexao {
 	public static void main(String[] args) throws SQLException {
-		
-		String stringConexao = "jdbc:mysql://localhost:3306";
+		//verifyServerCertificate=false&useSSL=true -> estou dizendo para fazer um conexao segura ssl mas não checar o cert
+		String stringConexao = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
 		String user = "root";
 		String senha = "sousa";
 		
